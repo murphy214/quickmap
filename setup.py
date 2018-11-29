4ba355cdc25697a181061d74995ee60e0443636d
Bennett Murphy <murphy214@live.marshall.edu>
2018-11-28 22:07:28 -0500

    eh

--- setup.py
+++ setup.py
@@ -11,5 +11,5 @@ setup(name='quickmap',
       #dependency_links=['http://github.com/murphy214/nlgeojson/tarball/master#egg=package-1.0'],#http://github.com/murphy214/pipeleaflet/tarball/master#egg=package-1.1','http://github.com/murphy214/pipevts/tarball/master#egg=package-1.0','http://github.com/murphy214/pipegls/tarball/master#egg=package-1.0']
       scripts = ['bin/map'],
       #dependency_links=['http://github.com/murphy214/nlgeojson.git#egg=nlgeojson-1.0']#http://github.com/murphy214/pipeleaflet/tarball/master#egg=package-1.1','http://github.com/murphy214/pipevts/tarball/master#egg=package-1.0','http://github.com/murphy214/pipegls/tarball/master#egg=package-1.0']
-
+      py_modules=['pipegls'],
       )
